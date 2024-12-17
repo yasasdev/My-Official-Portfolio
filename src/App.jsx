7244f6d8ddcc61f1def91b1febc5ad98fd0982d7
Yasasdev <yasaslekamge@gmail.com>
2024-12-18 00:39:29 +0530

    multiple files update

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// @copyright 2024 Yasas Lekamge
+// @license Apache-2.0
+
+const App = () => {
+    return <h1>Hello</h1>
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif']
+      }
+    },
   },
-  plugins: [],
+  plugins: [tailwindScrollbar],
 }
