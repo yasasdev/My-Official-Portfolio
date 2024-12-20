f11105849cba3de0938a7c77ca0377256e1050da
Yasasdev <yasaslekamge@gmail.com>
2024-12-20 20:10:39 +0530

    files update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-function Header() {
+const Header = () => {
   return (
-    <div>Header</div>
+    <header className="
+      fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+          <div className="max-w-screen-2xl w-full mx-auto px-4">
+              <h1>
+                  <a href="/" className="logo">
+                      <img src="images/logo.svg" width={40} height={40} alt="Yasas Lekamge" />
+                  </a>
+              </h1>
+          </div>
+    </header>
   )
 }
 
