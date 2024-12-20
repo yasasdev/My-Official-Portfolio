db3172b5bc6b182dad2839501e9de2dcd6c22795
Yasasdev <yasaslekamge@gmail.com>
2024-12-20 22:02:26 +0530

    multiple files update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,25 @@ const Header = () => {
   return (
     <header className="
       fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-          <div className="max-w-screen-2xl w-full mx-auto px-4">
+          <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6
+           md:grid md:grid-cols-[1fr,3fr,1fr]">
               <h1>
                   <a href="/" className="logo">
                       <img src="images/logo.svg" width={40} height={40} alt="Yasas Lekamge" />
                   </a>
               </h1>
+
+              <div className="relative md:justify-self-center">
+                <button className="menu-btn md:hidden" onClick={ null }>
+                  <span className="material-symbols-rounded">
+                    menu
+                  </span>
+                </button>
+                Navbar
+              </div>
+
+              <a href="#contact" className="">Contact Me</a>
+
           </div>
     </header>
   )
